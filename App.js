03c5f05ffa4df888e9f81891130d9c19dd72a16e
dmssantos <dms.santos_@hotmail.com>
2022-02-15 22:34:23 -0300

    update

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import foto from './src/assets/foto.jpeg';
 
 const App = () => {
+  function handleRedeSocial(rede_social) {
+    switch (rede_social) {
+      case 'linkedin':
+        Alert.alert('Meu linkedin', 'https://linkedin.com/in/davidsantos');
+        break;
+      case 'github':
+        Alert.alert('Meu github', 'https://github.com/in/davidsantos');
+        break;
+      case 'facebook':
+        Alert.alert('Meu facebook', 'https://facebook.com/in/davidsantos');
+        break;
+    }
+  }
   return (
     <>
       <View style={style.page}>
@@ -13,9 +33,41 @@ const App = () => {
           <Text style={style.nome}>DAVID SANTOS</Text>
           <Text style={style.funcao}>Desenvolvedor FullStack</Text>
           <View style={style.redes_sociais}>
-            <Icon name="github" size={30} />
-            <Icon name="facebook" size={30} />
-            <Icon name="linkedin" size={30} />
+            <TouchableOpacity onPress={() => handleRedeSocial('github')}>
+              <Icon name="github" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleRedeSocial('facebook')}>
+              <Icon name="facebook" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleRedeSocial('linkedin')}>
+              <Icon name="linkedin" size={30} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={style.card_container}>
+          <View style={style.card}>
+            <View style={style.card_header}>
+              <Text>Experiência Profissional</Text>
+            </View>
+            <View style={style.card_content}>
+              <Text style={style.card_content_text}>DMS desenvolvimento</Text>
+              <Text style={style.card_content_text}>DMS programador</Text>
+              <Text style={style.card_content_text}>DMS designer</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={style.card_container}>
+          <View style={style.card}>
+            <View style={style.card_header}>
+              <Text>Formação Academica</Text>
+            </View>
+            <View style={style.card_content}>
+              <Text style={style.card_content_text}>UNESA</Text>
+              <Text style={style.card_content_text}>DIO</Text>
+              <Text style={style.card_content_text}>RocketSeat</Text>
+            </View>
           </View>
         </View>
       </View>
@@ -49,9 +101,31 @@ const style = StyleSheet.create({
   },
   redes_sociais: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     width: '60%',
+    justifyContent: 'space-between',
     marginTop: 20,
+  },
+  card_container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 10,
+  },
+  card: {
+    width: '60%',
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#939393',
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  card_content: {
+    marginTop: 20,
+  },
+  card_content_text: {
+    color: '#939393',
+    marginBottom: 10,
   },
 });
 
